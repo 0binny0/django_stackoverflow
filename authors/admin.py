163959ad17400bb521927a6493e1f3fc131f7b48
Ben Hedgepeth <benhh@gmx.com>
2022-02-12 13:44:47 -0800

    created admin objects

--- authors/admin.py
+++ authors/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+from .models import Profile
 
 # Register your models here.
+
+class AdminUser(admin.ModelAdmin):
+
+    fields = ("username", )
+
+
+class AdminProfile(admin.ModelAdmin):
+
+    fields = ("user", )
+
+
+admin.site.register(get_user_model(), AdminUser)
+admin.site.register(Profile, AdminProfile)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Answer, Tag
+
+# class InlineAnswer(admin.StackedInline):
+#     model = Answer
+#     fieldsets = (
+#         "Answers", {
+#             "fields": (("date", "profile", "score"), "body"),
+#             "classes": ("collapse", )
+#         }
+#     )
+    #
+
+class AdminTag(admin.ModelAdmin):
+
+    fields = ('name',)
+
+
+class AdminQuestion(admin.ModelAdmin):
+
+    actions_on_bottom = False
+    actions_on_bottom = True
+
+    fieldsets = (
+        (
+            None, {
+                "fields": (("title", "profile", "date"), "tags", "body")
+            }
+        ),
+    )
+    exclude = ("comment", )
+    # readonly_fields = ("title", "body", "tags", "profile")
+
+    # inlines = [
+    #     InlineAnswer,
+    # ]
+
+
+admin.site.register(Question, AdminQuestion)
+admin.site.register(Tag, AdminTag)
+# admin.site.register(Answer, InlineAnswer)
