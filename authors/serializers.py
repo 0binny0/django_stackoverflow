b560fe7508414b6d83dc78700be3e24532ff8a5f
Ben Hedgepeth <benhh@gmx.com>
2022-03-05 13:04:38 -0800

    changed username field from RegexField to CharField

--- authors/serializers.py
+++ authors/serializers.py
@@ -15,14 +15,8 @@ from .validators import character_validator
 
 class LoginSerializer(ModelSerializer):
 
-    username = RegexField(
-        re.compile("^(_?[a-zA-Z0-9]+_?)+"),
-        min_length=6, max_length=20,
-        validators=[character_validator]
-    )
-
+    username = CharField(min_length=6, max_length=20)
     password = CharField(max_length=7, min_length=12)
-
 
     def validate(self, data):
         username_provided = data.get("username", None)
