ec990d4d0a88427f1a285cfa6cf0adb4b9d44d18
Ben Hedgepeth <benhh@gmx.com>
2023-01-31 11:57:47 -0800

    Added dynamic pagination buttons based on search results

--- authors/static/authors/js/listings.js
+++ authors/static/authors/js/listings.js
@@ -12,7 +12,7 @@ function set_user_listing_api_request(obj) {
     search = obj.value;
   }
 
-  const api_url = document.location.toString().replace(/users\/$/gi, `api/v1/users?search=${search}`);
+  const api_url = document.location.toString().replace(/users.*$/gi, `api/v1/users?search=${search}`);
   const request = new Request(api_url, {
     'method': "get",
     'headers': {
@@ -27,7 +27,15 @@ form.addEventListener("submit", function(event) {
   event.preventDefault()
 })
 
-function get_json_response(response) {
+window.addEventListener("DOMContentLoaded", function(event) {
+  const url_query_params = (new URL(document.location)).searchParams;
+
+  if (url_query_params.has("search")) {
+    user_search_query.value = url_query_params.get("search");
+  }
+})
+
+
   if (response.ok) {
     return response.json().then((data) => {
       data['http_status'] = response.status;
@@ -37,21 +45,22 @@ function get_json_response(response) {
   return {}
 }
 
-window.addEventListener("load", function(event) {
-  const current_url = window.location.href.replace(/(?<=users)\//ig, "");
-  window.location.href = current_url;
-
-})
+// window.addEventListener("load", function(event) {
+//   const current_url = window.location.href.replace(/(?<=users)\//ig, "");
+//   window.location.href = current_url;
+//
+// })
 
 
 user_search_query.addEventListener("keyup", function(event) {
   let page_warning_exists = document.querySelector("#no_users_warning");
   let page_listing = document.querySelector(".users_list");
   const pagination = document.querySelector(".main_pagination");
-  console.log(this.value);
   const request = set_user_listing_api_request(this);
   fetch(request).then(get_json_response).then((data) => {
-    if (Object.keys(data).length === 0) {
+    let users = data['users'];
+    let users_count = data['users'] ? Object.keys(data['users']).length : 0;
+    if (users_count === 0) {
       const message = `No users exist with the username ${this.value}`;
       if (page_listing) {
         let page_warning = document.createElement("h3");
@@ -66,19 +75,104 @@ user_search_query.addEventListener("keyup", function(event) {
       pagination.classList.add("hide");
     } else {
       pagination.classList.remove("hide");
-      let page_buttons = Array.from(document.getElementsByClassName("page_num"));
-      let paginated_buttons = Array.from(document.querySelectorAll(`a[id*=paginated_page]`));
-      const navigation_buttons = [...page_buttons, ...paginated_buttons];
-      page_buttons.forEach((page_button, index, array, user_search_query) => {
-        let paginated_link;
-        if (this.value) {
-          paginated_link = `${document.location.href.replace(/(?<=users)\//ig,'')}?page=${page_button.textContent}&search=${this.value}`;
-        } else {
-          paginated_link = `${document.location.href.replace(/(?<=users)\//ig,'')}?page=${page_button.textContent}`;
-        }
-        page_button.href = paginated_link
-      })
-      let users = data['users'];
+      users = users.slice(0, 15);
+      let active_page_nav_button_count = Math.ceil(users_count / 15);
+      let paginated_buttons = Array.from(document.querySelectorAll(`a[id*=paginated]`));
+      let numbered_page_buttons = Array.from(
+        document.getElementsByClassName("page_num")
+      );
+      const navigation_buttons = [...numbered_page_buttons, ...paginated_buttons];
+      const next_button_exists = navigation_buttons.find((button) => button.textContent === "Next");
+      if (next_button_exists === undefined) {
+        const next_button = document.createElement("a");
+        next_button.id = "paginated_page_next";
+        next_button.textContent = "Next";
+        next_button.classList.add("inactive_page");
+        navigation_buttons.push(next_button);
+      }
+      if (this.value) {
+        navigation_buttons.forEach((page_button, index, array, user_search_query) => {
+          let paginated_link;
+          const button_label = page_button.textContent;
+          if (button_label !== "Prev" && button_label !== "Next") {
+            const page_num = index + 1;
+            if (page_num > 5) {
+              page_button.style.display = "none";
+            } else {
+              page_button.textContent = `${page_num}`;
+              paginated_link = document.location.href.replace(/(?<=users)\/?.*/ig,`?page=${page_num}&search=${this.value}`);
+              page_button.href = paginated_link;
+              if (page_num === 1) {
+                page_button.style.cssText = "background: darkorange; color: white; border: 1px solid darkorange";
+              } else {
+                page_button.style.cssText = "background: white; color: black; border: 1px solid lightgrey";
+              }
+              if (page_num > active_page_nav_button_count) {
+                page_button.style.display = "none";
+              } else {
+                page_button.style.display = "inline-block";
+              }
+            }
+          } else if (button_label === "Next") {
+            if (active_page_nav_button_count > 1) {
+              paginated_link = document.location.href.replace(/(?<=users)\/?.*/ig,`?page=2&search=${this.value}`);
+              page_button.href = paginated_link;
+              page_button.classList.remove("hide")
+              // page_button.style.display = "inline-block";
+            } else {
+              page_button.classList.add("hide");
+              // page_button.style.display = "none";
+            }
+
+          } else {
+            if (button_label === "Prev") {
+              page_button.classList.add("hide");
+              // page_button.style.display = "none";
+            } else if (button_label === "Next") {
+              page_button.classList.remove("hide");
+              // page_button.style.display = "inline-block";
+            } else {
+              if (button_label === "1") {
+                page_button.style.cssText = `background: darkorange; color: white; border: 1px solid darkorange`;
+              } else {
+                page_button.style.cssText = `background: white; color: black; border: 1px solid lightgrey`;
+              }
+            }
+          }
+        })
+      } else {
+        navigation_buttons.forEach((page_button, index, array, user_search_query) => {
+          let paginated_link;
+          const button_label = page_button.textContent;
+          if (button_label === "Prev") {
+            page_button.style.display = "none";
+          } else if (button_label !== "Next") {
+            const page_num = index + 1;
+            if (page_num > 5) {
+              page_button.style.display = "none";
+            } else {
+              page_button.textContent = `${page_num}`;
+              paginated_link = document.location.href.replace(/(?<=users)\/?.*/ig,`?page=${page_num}`);
+              page_button.href = paginated_link;
+              if (page_num === 1) {
+                page_button.style.cssText = "background: darkorange; color: white; border: 1px solid darkorange; visibility: visible";
+              } else {
+                page_button.style.cssText = "background: white; color: black; border: 1px solid lightgrey; visibility: visible";
+              }
+            }
+          } else {
+            if (active_page_nav_button_count === 1) {
+              page_button.classList.add("hide");
+            } else {
+              let navigation_wrapper = document.querySelector(".page_through_wrapper");
+              const paginated_link = document.location.href.replace(/(?<=users)\/?.*/ig,`?page=2`);
+              page_button.href = paginated_link;
+              page_button.classList.remove("hide");
+              navigation_wrapper.appendChild(page_button);
+            }
+          }
+        })
+      }
       let new_displayed_users = [];
       for (let user of users) {
         let user_div_wrapper = document.createElement("div");
@@ -95,7 +189,7 @@ user_search_query.addEventListener("keyup", function(event) {
           user_stat.setAttribute("class", "user_subdata");
           if (key === "total_posts") {
             const posts = user['profile'][key];
-            user_stat.textContent = `${posts} ${posts > 1 ? 'posts': 'post'}`;
+            user_stat.textContent = `${posts} ${posts > 1 ? 'Posts': 'Post'}`;
           } else {
             const user_key = `${key.replaceAll(
               /(_|^)[a-z]/ig, (match) => {
@@ -110,6 +204,20 @@ user_search_query.addEventListener("keyup", function(event) {
         }
       }
       page_listing.replaceChildren(...new_displayed_users);
+      // if (users.length === 15) {
+      //   numbered_page_buttons.forEach((button, i, array) => {
+      //     button.style.visibility = "visible";
+      //   })
+      // } else {
+      //   active_page_buttons.forEach((button, i, array) => {
+      //     button.style.visibility = "visible";
+      //   })
+      //
+      //   inactive_page_buttons.forEach((button, i, array) => {
+      //     button.style.visibility = "hidden";
+      //   })
+      // }
+      // pagination.classList.remove("hide");
     }
   })
 
