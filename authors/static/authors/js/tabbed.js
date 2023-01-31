dcfdcf4b34513ba1909cd00f6c51e4c4f283951f
Ben Hedgepeth <benhh@gmx.com>
2023-01-31 00:23:27 -0800

    Styling query sort tab on user profile page

--- authors/static/authors/js/tabbed.js
+++ authors/static/authors/js/tabbed.js
@@ -4,21 +4,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const page_url = new URL(this.location);
   let query_string = new URLSearchParams(page_url.search);
   let index_query_tab = query_string.get("sort");
-  all_indexed_queries.forEach((query) => {
-    query.classList.remove("selected_index");
-  })
-  if (index_query_tab) {
-    index_query_tab = index_query_tab.toLowerCase();
-  } else {
-    index_query_tab = all_indexed_queries.at(0).textContent.toLowerCase();
-  }
-  let query_tab_match = all_indexed_queries.find((query) => {
-    return query.textContent.toLowerCase() === index_query_tab;
-  });
-  if (query_tab_match === undefined) {
-    const selected_query_index = all_indexed_queries.at(0);
-    selected_query_index.classList.add("selected_index");
-  } else {
-    query_tab_match.classList.add("selected_index");
+  if (all_indexed_queries.length !== 0) {
+    all_indexed_queries.forEach((query) => {
+      query.classList.remove("selected_index");
+    })
+    if (index_query_tab) {
+      index_query_tab = index_query_tab.toLowerCase();
+    } else {
+      index_query_tab = all_indexed_queries.at(0).textContent.toLowerCase();
+    }
+
+    let query_tab_match = all_indexed_queries.find((query) => {
+      return query.textContent.toLowerCase() === index_query_tab;
+    });
+    if (query_tab_match === undefined) {
+      const selected_query_index = all_indexed_queries.at(0);
+      selected_query_index.classList.add("selected_index");
+    } else {
+      query_tab_match.classList.add("selected_index");
+    }
   }
 })
