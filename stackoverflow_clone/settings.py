1d634f47b0f69b48f12ece9443c0f29c6f9bc293
Ben Hedgepeth <benhh@gmx.com>
2023-02-09 23:49:42 -0800

    Added config settings

--- stackoverflow_clone/settings.py
+++ stackoverflow_clone/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-from local_settings import *
+from .local_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
