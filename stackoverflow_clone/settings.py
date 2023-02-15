54dd2b95a489d9016a4fa8032c319ba369e3669f
Ben Hedgepeth <benhh@gmx.com>
2023-02-15 10:31:16 -0800

    changed key access from [] -> ()

--- stackoverflow_clone/settings.py
+++ stackoverflow_clone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = ["ittybitty.pythonanywhere.com", ]
 
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
