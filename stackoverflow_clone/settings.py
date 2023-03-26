2c33e62392cbb58da9807ea3171f8c3fd0b1e836
Ben Hedgepeth <benhh@gmx.com>
2023-03-25 18:18:45 -0700

    Added python-dotenv to requirements

--- stackoverflow_clone/settings.py
+++ stackoverflow_clone/settings.py
@@ -13,16 +13,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-from decouple import config
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ["ittybitty.pythonanywhere.com", ]
-
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get("secret_key")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -31,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get("debug", "true").upper() == "TRUE")
 if DEBUG:
     import mimetypes
     mimetypes.add_type("text/javascript", ".js")
@@ -94,8 +93,11 @@ WSGI_APPLICATION = 'stackoverflow_clone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("engine", 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get("db_name", BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get("db_user", ""),
+        'PASSWORD': os.environ.get('db_password', ""),
+        'HOST': os.environ.get('db_host', "")
     }
 }
 
